bf85a5e018fd75ef6d5ba5cb80bf0aced364d5e6
ankitt01 <ankitrawat0102@gmail.com>
2021-05-03 20:13:26 +0530

    refactor

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import banner from './img/banner.jpg';
 import './style/hero.css';
-import ProfileCard from './profile-card'
+import ProfileCard from './profile-card';
+import Settings from './Settings';
 
 class Hero extends Component {
     state = {  }
@@ -11,7 +12,12 @@ class Hero extends Component {
                 <img className="banner-img" src={banner} alt="banner"/>
                 <div className="feed">
                     <ProfileCard />
-
+                    <div className="setting-cards">
+                        <Settings label="Activity" source="https://img.icons8.com/ios-glyphs/26/000000/visible--v1.png" />
+                        <Settings label="Moments" source="https://img.icons8.com/material/26/000000/the-flash-sign.png" />
+                        <Settings label="Friends" source="https://img.icons8.com/ios-filled/26/000000/friends.png" />
+                        <Settings label="Edit Profile" source="https://img.icons8.com/android/26/000000/settings.png" />
+                    </div>
                 </div>
             </div>
         );
